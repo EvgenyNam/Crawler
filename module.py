d6b7ec1fd9942ed3612038771b2802b9dc22da11
EvgenyNam <evgeny.nam.2012@gmail.com>
2018-09-01 00:41:43 -0400

    added requests lib

--- module.py
+++ module.py
@@ -1,6 +1,17 @@
 import httplib
-conn = httplib.HTTPConnection("www.google.com")
-conn.request("GET","/index.html")
-res = conn.getresponse()
-print(res.read())
-print res.status, res.reason
+import requests
+
+# conn = httplib.HTTPConnection("www.google.com")
+# conn.request("GET","/index.html")
+# res = conn.getresponse()
+# print(res.read())
+# print res.status, res.reason
+
+class Http:
+	def getRequest(self, url):
+		resp = requests.get(url)
+		return resp
+
+http = Http()
+resp = http.getRequest('http://www.google.com')
+print(resp.text)
