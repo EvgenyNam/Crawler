16bd2d5710a68a3e508c573ffe05d7de87f039a0
EvgenyNam <evgeny.nam.2012@gmail.com>
2018-09-03 18:37:15 -0400

    formatting fix

--- Http/http.py
+++ Http/http.py
@@ -1,8 +1,5 @@
 import requests
 
-#
-# Perform custom google search with low level criteria and have a crawler filter it out
-#
 class Object(object):
     pass
 
@@ -10,10 +7,8 @@ class Http:
     def getRequest(self, url):
         try:
             resp = requests.get(url)
-
             return resp
         except:
             errorObj = Object()
             errorObj.status_code = 404
-
-        return errorObj
+            return errorObj
